bd54ad6178ddfa4604272d7427ed3a820cc181bc
Duy Nguyen <duy.nguyenkhanh@aegona.com>
2024-01-01 16:09:01 +0700

    update docker file

---
+++ build-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo docker kill coupon_web_portal
+sudo docker rm coupon_web_portal
+
+sudo docker build -t coupon_web_portal .
+
+sudo docker run -d --network blueone-net -p 3101:80 --name coupon_web_portal coupon_web_portal
