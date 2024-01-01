459cbf502fe957256b1ef0b00aa097e4dc177359
Duy Nguyen <duy.nguyenkhanh@aegona.com>
2024-01-01 16:34:31 +0700

    update docker

--- build-docker.sh
+++ build-docker.sh
@@ -4,4 +4,4 @@ sudo docker rm coupon_web_portal
 
 sudo docker build -t coupon_web_portal .
 
-sudo docker run -d --network blueone-net -p 3101:80 --name coupon_web_portal coupon_web_portal
+sudo docker run -d --network home-net -p 3101:80 --name coupon_web_portal coupon_web_portal
