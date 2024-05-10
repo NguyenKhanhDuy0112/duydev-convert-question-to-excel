ac8e40724467a129cc9b979f09586eba5bf59b17
Duy Nguyen <duy.nguyenkhanh@aegona.com>
2024-05-10 22:20:08 +0700

    update dockerfile

--- build-docker.sh
+++ build-docker.sh
@@ -2,6 +2,6 @@
 sudo docker kill coupon_web_portal
 sudo docker rm coupon_web_portal
 
-sudo docker build -t coupon_web_portal .
+sudo docker build -t coupon_web_portal --build-arg ENV_BUILD_WIDGET -f .
 
-sudo docker run -d --network home-net -p 3101:80 --name coupon_web_portal coupon_web_portal
+sudo docker run -d --network k2hitech-net -p 3100:80 --name coupon_web_portal coupon_web_portal
