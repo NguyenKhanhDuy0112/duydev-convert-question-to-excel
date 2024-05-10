ddd14520d1fec7e946c2f9f1059e7dde73a3e37d
Duy Nguyen <duy.nguyenkhanh@aegona.com>
2024-05-10 21:32:04 +0700

    update dockerfile

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ import ManageRoutes from "./routes"
 import { Provider } from "react-redux"
 import { App as AppAntd } from "antd"
 import store from "@/redux/index.store"
+import { env } from "./constants"
 
 function App() {
+    console.log("REACT_APP_API_BO_ENDPOINT: ", env.API_BO_ENDPOINT)
     return (
         <Provider store={store}>
             <ConfigProvider
