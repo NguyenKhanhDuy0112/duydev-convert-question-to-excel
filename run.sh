13f5b066316f639a9f22f85837b32c4703214b5e
Duy Nguyen <duy.nguyenkhanh@aegona.com>
2024-05-18 22:32:02 +0700

    update docker file

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 
 echo "Regenerate Application Configuration"
 
+echo "Environment Variables in Container:"
+env | sort
+
 # Inject environment variables using a tool like `sed` (replace with your preferred tool):
 sed -i "s/REACT_APP_API_BO_ENDPOINT=.*$/REACT_APP_API_BO_ENDPOINT=${REACT_APP_API_BO_ENDPOINT}/g" ./build/static/js/*.js
 
