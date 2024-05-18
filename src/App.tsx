0e67c74b5820de49cd3eeff5766cea91dd0796f5
Duy Nguyen <duy.nguyenkhanh@aegona.com>
2024-05-18 23:13:02 +0700

    update docker file

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,8 @@ import { Provider } from "react-redux"
 import { App as AppAntd } from "antd"
 import store from "@/redux/index.store"
 import { env } from "./constants"
-import { API_ENDPOINT } from "./configs"
 function App() {
-    console.log("REACT_APP_API_BO_ENDPOINT: ", API_ENDPOINT, "env: ", env.API_BO_ENDPOINT)
+    console.log("REACT_APP_API_BO_ENDPOINT: ", "env: ", env.API_BO_ENDPOINT)
     return (
         <Provider store={store}>
             <ConfigProvider
